e2ba684e1c1fa47925a34574b8c91eb7af9dc030
Yusuke Shiya <yusuke.shiya@gmail.com>
2023-12-17 23:26:13 +0900

    fix: タイポ

--- lib/domain/user/user_repository.dart
+++ lib/domain/user/user_repository.dart
@@ -1,6 +1,6 @@
 import 'package:self_manage_app/domain/user/entity/user.dart';
 
-abstract class AuthRepository {
+abstract class UserRepository {
   Future<User> signIn(String email, String password);
   Future<User> signUp(String email, String password);
   Future<void> signOut();
