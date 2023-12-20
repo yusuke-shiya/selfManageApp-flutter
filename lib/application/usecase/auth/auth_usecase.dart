da420c8d217752be08e3d8c07af72857ad0ac5e7
Yusuke Shiya <yusuke.shiya@gmail.com>
2023-12-20 22:35:30 +0900

    feat: application層を実装

---
+++ lib/application/usecase/auth/auth_usecase.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:self_manage_app/domain/user/entity/user.dart';
+import 'package:self_manage_app/domain/user/user_repository.dart';
+// TODO: infrastracture層に依存してしまっているので、domain層に依存させるように修正したい
+import 'package:self_manage_app/infrastracture/firebase/firebase_auth_repository.dart';
+
+class AuthUseCase {
+  final UserRepository userRepository;
+
+  AuthUseCase(this.userRepository);
+
+  Future<User> signIn(String email, String password) async {
+    return userRepository.signIn(email, password);
+  }
+
+  Future<User> signUp(String email, String password) async {
+    return userRepository.signUp(email, password);
+  }
+
+  Future<void> signOut() async {
+    await userRepository.signOut();
+  }
+}
+
+final authUseCaseProvider = Provider<AuthUseCase>((ref) {
+  return AuthUseCase(ref.read(userRepositoryProvider));
+});
