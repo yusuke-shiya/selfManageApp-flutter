eaea0348748d8abf4d853c201776fb591acb7993
Yusuke Shiya <yusuke.shiya@gmail.com>
2024-01-08 21:03:51 +0900

    feat: DI修正

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:self_manage_app/presentation/page/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:self_manage_app/domain/user/user_repository.dart';
+import 'package:self_manage_app/domain/auth/auth_repository.dart';
 import 'package:self_manage_app/infrastracture/firebase/firebase_auth_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -16,7 +16,7 @@ void main() async {
   runApp(
     ProviderScope(
       overrides: [
-        userRepositoryProvider.overrideWithValue(
+        authRepositoryProvider.overrideWithValue(
           FirebaseAuthRepository(
             FirebaseAuth.instance,
           ),
