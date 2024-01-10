5e92c0c76e7bd4243d2ed445cad6bb30c2b5b441
Yusuke Shiya <yusuke.shiya@gmail.com>
2024-01-10 23:22:09 +0900

    feat: user_provider.dartをAsyncNotifierを使う形に置き換え

--- lib/application/usecase/user/state/user_provider.dart
+++ lib/application/usecase/user/state/user_provider.dart
@@ -1,57 +1,38 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:self_manage_app/domain/user/entity/user.dart';
 import 'package:self_manage_app/application/usecase/user/user_usecase.dart';
+import 'package:self_manage_app/domain/user/entity/user.dart';
 
-// ユーザー情報を管理するクラス
-class UserState {
-  final User? user;
-  final bool isLoading;
-  final String? error;
+final userProvider =
+    AsyncNotifierProvider<UserNotifier, User>(UserNotifier.new);
 
-  UserState({this.user, this.isLoading = false, this.error});
+class UserNotifier extends AsyncNotifier<User> {
+  @override
+  User build() {
+    return const User(
+      userUuid: '',
+      email: '',
+      name: '',
+    );
+  }
 
-  // ユーザーを取得した状態にするためのファクトリコンストラクタ
-  UserState.user(User user) : this(user: user);
-
-  // エラーがある状態にするためのファクトリコンストラクタ
-  UserState.error(String error) : this(error: error);
-
-  // ローディング状態にするためのファクトリコンストラクタ
-  UserState.loading() : this(isLoading: true);
-}
-
-// ユーザー情報を管理するプロバイダ
-final userStateProvider =
-    StateNotifierProvider<UserStateNotifier, UserState>((ref) {
-  final userUseCase = ref.read(userUseCaseProvider);
-  return UserStateNotifier(userUseCase);
-});
-
-// StateNotifierを継承したクラスで状態のロジックを扱う
-class UserStateNotifier extends StateNotifier<UserState> {
-  final UserUseCase _userUseCase;
-
-  UserStateNotifier(this._userUseCase) : super(UserState());
-
-  // ユーザー情報を取得するロジック
   Future<void> get(String token) async {
+    state = const AsyncValue.loading();
     try {
-      state = UserState.loading();
-      final user = await _userUseCase.get(token);
-      state = UserState.user(user);
+      final user = await ref.read(userUseCaseProvider).get(token);
+      state = AsyncValue.data(user);
     } catch (e) {
-      state = UserState.error(e.toString());
+      state = AsyncValue.error(e, StackTrace.current);
     }
   }
 
-  // ユーザー作成のロジック
   Future<void> create(String name, String email, String token) async {
+    state = const AsyncValue.loading();
     try {
-      state = UserState.loading();
-      final user = await _userUseCase.create(name, email, token);
-      state = UserState.user(user);
+      final user =
+          await ref.read(userUseCaseProvider).create(name, email, token);
+      state = AsyncValue.data(user);
     } catch (e) {
-      state = UserState.error(e.toString());
+      state = AsyncValue.error(e, StackTrace.current);
     }
   }
 }
