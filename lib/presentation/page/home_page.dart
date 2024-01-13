27d0bb6822324f850674f52de8fc50bc4cdbcbd6
Yusuke Shiya <yusuke.shiya@gmail.com>
2024-01-13 16:47:51 +0900

    feat: ホーム画面でexpanse_page.dartを呼び出し

--- lib/presentation/page/home_page.dart
+++ lib/presentation/page/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:self_manage_app/application/usecase/auth/state/auth_provider.dart';
 import 'package:self_manage_app/application/usecase/user/state/user_provider.dart';
+import 'package:self_manage_app/presentation/page/expense/expense_page.dart';
 import 'package:self_manage_app/presentation/page/user/signin_page.dart';
 
 class HomePage extends ConsumerWidget {
@@ -19,34 +20,6 @@ class HomePage extends ConsumerWidget {
       });
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('ホーム'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('name: ${userState.asData?.value.name ?? ''}'),
-            const SizedBox(height: 10),
-            Text('email: ${userState.asData?.value.email ?? ''}'),
-            const SizedBox(height: 10),
-            Text('user uuid: ${userState.asData?.value.userUuid ?? ''}'),
-            const SizedBox(height: 10),
-            Text('auth uid: ${authState.auth?.uid ?? ''}'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              child: Text('ログアウト'),
-              onPressed: () async {
-                await ref.read(authStateProvider.notifier).signOut();
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) => SigninPage()),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+    return const ExpensePage();
   }
 }
