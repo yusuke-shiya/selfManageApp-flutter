643b177b9afc25ada602a4e2136aac79e599dd5b
Yusuke Shiya <yusuke.shiya@gmail.com>
2024-02-03 21:57:58 +0900

    fix: 誤った引数

--- lib/domain/expense/expense_repository.dart
+++ lib/domain/expense/expense_repository.dart
@@ -2,7 +2,8 @@ import 'package:self_manage_app/domain/expense/entity/income.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 abstract class ExpenseRepository {
-  Future<Income> createIncome(String name, String email, String token);
+  Future<Income> createIncome(
+      String year, String month, String amount, String token);
 }
 
 final expensiveRepositoryProvider = Provider<ExpenseRepository>((ref) {
