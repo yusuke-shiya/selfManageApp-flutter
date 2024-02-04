e5fcea86dbef79d9727f98f295cd7a9d254636a3
Yusuke Shiya <yusuke.shiya@gmail.com>
2024-02-04 19:36:15 +0900

    feat: outcomeの登録処理を実装

--- lib/domain/expense/expense_repository.dart
+++ lib/domain/expense/expense_repository.dart
@@ -1,8 +1,11 @@
 import 'package:self_manage_app/domain/expense/entity/income.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:self_manage_app/domain/expense/entity/outcome.dart';
 
 abstract class ExpenseRepository {
   Future<Income> createIncome(int year, int month, int amount, String token);
+  Future<Outcome> createOutcome(
+      int year, int month, int day, int amount, String title, String token);
 }
 
 final expensiveRepositoryProvider = Provider<ExpenseRepository>((ref) {
--- lib/infrastracture/api/expensive_api_repository.dart
+++ lib/infrastracture/api/expensive_api_repository.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:self_manage_app/domain/expense/entity/income.dart';
+import 'package:self_manage_app/domain/expense/entity/outcome.dart';
 import 'package:self_manage_app/domain/expense/expense_repository.dart';
 
 const url = 'https://selfmanageapp.onrender.com/expense';
@@ -28,6 +29,38 @@ class ExpenseApiRepository implements ExpenseRepository {
       );
     } else {
       throw Exception('Failed to create income');
+    }
+  }
+
+  @override
+  Future<Outcome> createOutcome(int year, int month, int day, int amount,
+      String title, String token) async {
+    final response = await _dio.post(
+      url,
+      data: {
+        'year': year,
+        'month': month,
+        'day': day,
+        'amount': amount,
+        'title': title
+      },
+      options: Options(
+        headers: {
+          'Authorization': token,
+        },
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      return Outcome(
+        year: year,
+        month: month,
+        day: day,
+        amount: amount,
+        title: title,
+      );
+    } else {
+      throw Exception('Failed to create outcome');
     }
   }
 }
